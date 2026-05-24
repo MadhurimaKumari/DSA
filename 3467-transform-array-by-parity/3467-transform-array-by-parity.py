9999782c4f37c4bcef15159116aeb6efed90713f
MadhurimaKumari <kumarimadhurima785@gmail.com>
2026-05-24 14:39:09 +0530

    Time: 0 ms (100%), Space: 19.2 MB (55.29%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.py
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.py
@@ -1,11 +1,10 @@
 class Solution:
     def transformArray(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            if nums[i]%2==0:
-                nums[i]=0
-            else:
-                nums[i]=1
-            
-        
-        nums.sort()
-        return nums
+        # for i in range(len(nums)):
+        #     if nums[i]%2==0:
+        #         nums[i]=0
+        #     else:
+        #         nums[i]=1
+        # nums.sort()
+        # return nums
+        return sorted([x%2 for x in nums])
