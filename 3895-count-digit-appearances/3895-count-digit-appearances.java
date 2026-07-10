02fb4f761ec6b27e2f15b2fedb724aa24a564d87
MadhurimaKumari <kumarimadhurima785@gmail.com>
2026-07-10 17:20:00 +0530

    Time: 13 ms (5.14%), Space: 46.9 MB (55.76%) - LeetHub

--- 3895-count-digit-appearances/3895-count-digit-appearances.java
+++ 3895-count-digit-appearances/3895-count-digit-appearances.java
@@ -1,14 +1,25 @@
 class Solution {
     public int countDigitOccurrences(int[] nums, int digit) {
-        int sum=0;
-        for(int i:nums){
-        String s=String.valueOf(i);
-            for(int j=0;j<s.length();j++){
-                if(s.charAt(j)==(char)(digit+'0')){
-                    sum++;
-                }
-            }
-        }
-        return sum;
+    //     int sum=0;
+    //     for(int i:nums){
+    //     String s=String.valueOf(i);
+    //         for(int j=0;j<s.length();j++){
+    //             if(s.charAt(j)==(char)(digit+'0')){
+    //                 sum++;
+    //             }
+    //         }
+    //     }
+    //     return sum;
+    // 
+    
+
+
+    int ans=0;
+    for(int num:nums){
+        String s=String.valueOf(num);
+        ans+=s.length()-s.replace(String.valueOf(digit),"").length();
+    }
+    return ans;
+    
     }
 }
